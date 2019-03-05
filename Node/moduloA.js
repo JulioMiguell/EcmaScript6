1241089d1abd3b42143ba068a8250fc6536a0941
juliomiguel <juliomiguelsouzacosta@gmail.com>
2019-03-04 21:21:43 -0300

    pequenos exeplos com modulos

---
+++ Node/moduloA.js
@@ -0,0 +1,5 @@
+console.log(this) // this == {} objeto vazio
+
+this.Ola = 'Hello World'
+exports.bemVindo = 'Bem-vindo ao node'
+module.exports.ateLogo = 'Até o próximo exemplo'
---
+++ Node/moduloCliente.js
@@ -0,0 +1,10 @@
+const moduloA = require('./moduloA') // ./ caminho relativo
+const moduloB = require('./moduloB')
+
+console.log(moduloA.ateLogo)
+console.log(moduloA.bemVindo)
+console.log(moduloA.Ola)
+console.log(moduloA)
+
+console.log(moduloB.boaNoite)
+console.log(moduloB)
